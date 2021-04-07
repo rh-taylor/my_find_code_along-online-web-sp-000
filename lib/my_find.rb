bf1a063cd797067eadf5cbe9ebe4e81eea18613f
Robyn Taylor <rhtaylor0715@gmail.com>
2021-04-07 16:58:25 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,13 +3,9 @@ require 'pry'
 def my_find(collection)
   i = 0
   while i < collection.length
-    yield(collection[i])
-    i += 1
-    if collection[i] == true
-      break
-    else
-      yield(collection[i])
-      i += 1
+    if yield(collection[i])
+      return collection[i]
     end
+    i += 1
   end
 end
